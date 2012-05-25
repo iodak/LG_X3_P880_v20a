a4a7ef6bfc39c57a53d968354dd14493031148e4
faux123 <reioux@gmail.com>
2012-05-25 14:03:22 -0700

    tegra: custom user voltage control
    initial implementation inspired by coolbho3k's custom voltage control for
    galaxy nexus (OMAP4460)
    
    also added some Processor ID debugging code to properly identify tegra3
    variants
    
    Conflicts:
    	arch/arm/mach-tegra/tegra3_dvfs.c
    	drivers/cpufreq/cpufreq.c

--- arch/arm/mach-tegra/tegra3_dvfs.c
+++ arch/arm/mach-tegra/tegra3_dvfs.c
@@ -29,8 +29,15 @@
 #include "board.h"
 #include "tegra3_emc.h"
 
+
 #define CPU_MILLIVOLTS {\
 	750, 762, 775, 787, 800, 825, 837, 850, 862, 875, 887, 900, 912, 916, 925, 937, 950, 962, 975, 987, 1000, 1007, 1012, 1025, 1037, 1050, 1062, 1075, 1087, 1100, 1112, 1125, 1137, 1150, 1162, 1175, 1187, 1200, 1212, 1237};
+
+#ifdef CONFIG_VOLTAGE_CONTROL
+int user_mv_table[MAX_DVFS_FREQS] = {
+	800, 825, 850, 875, 900, 912, 975, 1000, 1025, 1050, 1075, 1100, 1125, 1150, 1175, 1200, 1212, 1237};
+#endif
+
 
 static bool tegra_dvfs_cpu_disabled;
 static bool tegra_dvfs_core_disabled;
@@ -58,12 +65,14 @@ static int cpu_below_core = VDD_CPU_BELOW_VDD_CORE;
 
 static struct dvfs_rail tegra3_dvfs_rail_vdd_cpu = {
 	.reg_id = "vdd_cpu",
-	.max_millivolts = 1250,
 #ifdef CONFIG_MACH_X3
 	.min_millivolts = 800,
 #else
 	.min_millivolts = 725,
 #endif
+	.max_millivolts = 1300,
+	.min_millivolts = 850,
+
 	.step = VDD_SAFE_STEP,
 	.jmp_to_zero = true,
 };
@@ -622,7 +631,7 @@ static void __init init_cpu_0_dvfs(struct dvfs *cpud)
 static int __init get_cpu_nominal_mv_index(
 	int speedo_id, int process_id, struct dvfs **cpu_dvfs)
 {
-	int i, j, mv;
+	int i, j, mv, nom_index;
 	struct dvfs *d;
 	struct clk *c;
 
@@ -639,8 +648,10 @@ static int __init get_cpu_nominal_mv_index(
 	}
 	BUG_ON(i == 0);
 	mv = cpu_millivolts[i - 1];
+	pr_info("cpu_nominal_mv: %i\n", mv);
 	BUG_ON(mv < tegra3_dvfs_rail_vdd_cpu.min_millivolts);
 	mv = min(mv, tegra_cpu_speedo_mv());
+	pr_info("cpu_nominal_mv_min: %i\n", mv);
 
 	/*
 	 * Find matching cpu dvfs entry, and use it to determine index to the
@@ -670,6 +681,8 @@ static int __init get_cpu_nominal_mv_index(
 		}
 	}
 
+	pr_info("dvfs: freqs_mult: %i\n", d->freqs_mult);
+
 	BUG_ON(i == 0);
 	if (j == (ARRAY_SIZE(cpu_dvfs_table) - 1))
 		pr_err("tegra3_dvfs: WARNING!!!\n"
@@ -679,7 +692,17 @@ static int __init get_cpu_nominal_mv_index(
 		       speedo_id, process_id, d->freqs[i-1] * d->freqs_mult);
 
 	*cpu_dvfs = d;
-	return (i - 1);
+
+	nom_index = i - 1;
+	pr_info("cpu_nominal_mv_index: %i\n", nom_index);
+
+	pr_info("cpu_dvfs->speedo_id: %i\n", d->speedo_id);
+	pr_info("cpu_dvfs->process_id: %i\n", d->process_id);
+	for (i=0;i<MAX_DVFS_FREQS;i++) {
+		pr_info("cpu_dvfs->freqs: %lu\n", d->freqs[i]);
+	}
+
+	return nom_index;
 }
 
 static int __init get_core_nominal_mv_index(int speedo_id)
