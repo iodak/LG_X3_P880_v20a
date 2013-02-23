bfac85cae1505ddfda41da8382df7414fa49d152
maxl <maxl@4b8ad20e-30ca-41b1-81b8-3c8b9e269e15>
2013-02-23 23:18:27 +0000

    sched: Kill WAKEUP_PREEMPT
    Remove the WAKEUP_PREEMPT feature, disabling it doesn't make any sense
    and its outlived its use by a long long while.
    
    Change-Id: I36e5e74af9c6f5d32b92e5334c4f735a39fe962c

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -1124,9 +1124,6 @@ check_preempt_tick(struct cfs_rq *cfs_rq, struct sched_entity *curr)
 	 * narrow margin doesn't have to wait for a full slice.
 	 * This also mitigates buddy induced latencies under load.
 	 */
-	if (!sched_feat(WAKEUP_PREEMPT))
-		return;
-
 	if (delta_exec < sysctl_sched_min_granularity)
 		return;
 
@@ -1267,7 +1264,7 @@ entity_tick(struct cfs_rq *cfs_rq, struct sched_entity *curr, int queued)
 		return;
 #endif
 
-	if (cfs_rq->nr_running > 1 || !sched_feat(WAKEUP_PREEMPT))
+	if (cfs_rq->nr_running > 1)
 		check_preempt_tick(cfs_rq, curr);
 }
 
@@ -2551,10 +2548,6 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 	 * is driven by the tick):
 	 */
 	if (unlikely(p->policy != SCHED_NORMAL))
-		return;
-
-
-	if (!sched_feat(WAKEUP_PREEMPT))
 		return;
 
 	find_matching_se(&se, &pse);
