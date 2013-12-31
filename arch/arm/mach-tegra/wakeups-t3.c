6c81b8a2856342f5213a03a2ee413dc10f0fd583
iodak <o93.ivan@gmail.com>
2013-12-31 02:17:59 +0100

    Revert "mach-tegra: wakeups: DT2W:  add lge_touch IRQ as a wake up source"
    This reverts commit 623b0073986cbf628e514ac7c516de1118ac5997.

--- arch/arm/mach-tegra/wakeups-t3.c
+++ arch/arm/mach-tegra/wakeups-t3.c
@@ -82,7 +82,6 @@ static struct tegra_wake_info tegra_wake_event_data_t3[] = {
 #if 0 /*                                      */
 	{INT_USB2, POLARITY_LEVEL_HI}, /* TEGRA_USB2_UHSIC, */	/* wake42 */
 #endif	
-	{TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PQ3), POLARITY_NONE},	/* wake42 */
 };
 
 struct tegra_wake_info *tegra_wake_event_data = tegra_wake_event_data_t3;
